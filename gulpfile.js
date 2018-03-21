997e8cb5344e7a997bc738bf87d8c5c449378428
zhouqiang <970059509@qq.com>
2018-03-21 18:12:06 +0800

    更新文件

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,19 @@
 var sass = require('gulp-sass');
 var gulp = require('gulp');
 
-gulp.task('default', function() {
-    // 将你的默认的任务代码放在这
+// 创建sass转css任务
+gulp.task('sass',function(){
+    // 这个任务负责把scss文件转成css
     return gulp.src('./sass/**/*.scss')
         .pipe(sass.sync().on('error', sass.logError))
         .pipe(gulp.dest('./css'));
 });
+
+// 创建监听任务watch
+gulp.task('watch',function(){
+    // 监听sass文件夹下所有.scss文件的变化，变化了就重新执行sass转css任务
+    gulp.watch('./sass/**/*.scss',['sass']);
+});
+
+// 创建default任务 运行这个任务的时候执行sass任务和watch任务
+gulp.task('default', ['sass','watch']);
