7b5333eb6cef77d633bd9a94497ff6756eb96ca2
zhouqiang <970059509@qq.com>
2018-03-21 21:38:44 +0800

    更新文件

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,22 @@
 var sass = require('gulp-sass');
 var gulp = require('gulp');
+var del = require('del');
+var autoprefixer = require('gulp-autoprefixer');
+
+//创建一个删除文件夹任务
+gulp.task('del', function () {
+    del.sync(['./css']);
+});
 
 // 创建sass转css任务
 gulp.task('sass',function(){
     // 这个任务负责把scss文件转成css
     return gulp.src('./sass/**/*.scss')
-        .pipe(sass.sync().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false,
+        }))
+        .pipe(sass.sync({outputStyle: 'compact'}).on('error', sass.logError))
         .pipe(gulp.dest('./css'));
 });
 
@@ -16,4 +27,5 @@ gulp.task('watch',function(){
 });
 
 // 创建default任务 运行这个任务的时候执行sass任务和watch任务
-gulp.task('default', ['sass','watch']);
+gulp.task('default', ['del','sass','watch']);
+
